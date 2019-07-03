afdc605fabee4a6d92455e2ba361946c99015f26
Rico <zhenchaozhan@gmail.com>
2019-07-03 21:03:28 +0800

    更细main.js的配置

--- src/main.js
+++ src/main.js
@@ -20,71 +20,74 @@ Vue.use(VueCookie);
 Vue.config.productionTip = false
 
 router.beforeEach( async (to, from, next) => {
-  // todo:获取cookie是否有openID
-  // 有：查询用户信息
-  // 无：授权（动态授权，静态授权）
-  // 获取code（to.query.code）之后全局保存用户对象，cookie保存openID
   let openID = VueCookie.get("openID");
   let code =to.query.code;
   const axios = require('axios');
+  let user = store.state.user
 
-  openID = "oHDTCwX3Ql7BkN5zeJQFFmlbHECY"
-
-  if (openID){
+  if (user){
+    if (to.name === "clothes") {
+      // console.log(user.clothesJson)
+      if (user.clothesJson) {
+        next({ path: '/ranking' })
+      }else {
+        next()
+      }
+    }
+  } else if (openID) {
     await axios.get('/bmw/api/user/' + openID)
-        .then(function (response) {
-            let result = response.data
-            if (result.status === 1) {
-                let user = result.data;
-                store.commit("setUser", user)
-                VueCookie.set("openID", user.openID)
-                if (from.name === "home") {
-                    // console.log(user.clothesJson)
-                    if (user.clothesJson) {
-                        next({ path: '/ranking' })
-                    }else {
-                        next()
-                    }
-                }
-            } else {
-                console.log(result.msg)
+      .then(function (response) {
+        let result = response.data
+        if (result.status === 1) {
+          let user = result.data;
+          store.commit("setUser", user)
+          VueCookie.set("openID", user.openID)
+          if (to.name === "clothes") {
+            // console.log(user.clothesJson)
+            if (user.clothesJson) {
+              next({ path: '/ranking' })
+            }else {
+              next()
             }
-
-        });
+          }
+        } else {
+          console.log(result.msg)
+        }
+      });
   } else {
     //  静态授权
     if(to.name === "vote"){
       await axios.get('/bmw/api/mp/oauth2/base?code=' + code)
-          .then(function (response) {
-              let result = response.data
-              if (result.status === 1) {
-                  let openID = result.data;
-                  VueCookie.set("openID", openID)
-              } else {
-                  console.log(result.msg)
-              }
-          })
+        .then(function (response) {
+          let result = response.data
+          if (result.status === 1) {
+            let openID = result.data;
+            VueCookie.set("openID", openID)
+          } else {
+            console.log(result.msg)
+          }
+        })
     } else {
       await axios.get('/bmw/api/mp/oauth2?code=' + code)
-          .then(function (response) {
-              let result = response.data
-              if (result.status === 1) {
-                  let user = result.data;
-                  store.commit("setUser", user)
-                  VueCookie.set("openID", user.openID)
-                  if (from.name === "home") {
-                      console.log(user.clothesJson)
-                      if (user.clothesJson) {
-                          next({ path: '/ranking' })
-                      }else {
-                          next()
-                      }
-                  }
-              } else {
-                  console.log(result.msg)
+        .then(function (response) {
+          let result = response.data
+          if (result.status === 1) {
+            let user = result.data;
+            store.commit("setUser", user)
+            VueCookie.set("openID", user.openID)
+            if (from.name === "home") {
+              console.log(user.clothesJson)
+              if (user.clothesJson) {
+                next({ path: '/ranking' })
+              }else {
+                next()
               }
-            // store.commit()
-          })
+            }
+          } else {
+            console.log(result.msg)
+          }
+          // store.commit()
+        })
     }
   }
   next()
