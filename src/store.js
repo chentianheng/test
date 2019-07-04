3d6446148d37f6d2fad9b7523bf17dd944d67f92
Rico <zhenchaozhan@gmail.com>
2019-07-04 19:06:23 +0800

    更新修复分享功能

--- src/store.js
+++ src/store.js
@@ -85,7 +85,7 @@ export default new Vuex.Store({
       }).then(response => {
         let data = response.data;
         if (data.status === 1) {
-          console.log(state.url)
+          alert(state.url)
           let result = data.data
           let wxConfig = {
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
