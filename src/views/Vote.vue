1e1ee719a1e9fb7d1536c5f54b216425d00622aa
Rico <zhenchaozhan@gmail.com>
2019-07-04 16:57:19 +0800

    更新接口权限

--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -146,7 +146,7 @@
         this.option = {
           title: '全城寻求潮流达人', // 分享标题, 请自行替换
           desc: '全新BMW 3系广州发布会', // 分享描述, 请自行替换
-          link: location.href.split('#')[0], // 分享链接，根据自身项目决定是否需要split
+          link: encodeURIComponent("http://binarytre.com?openID=" + this.openID), // 分享链接，根据自身项目决定是否需要split
           imgUrl: "https://mo.bintre.com/bmw.png" // 分享图标, 请自行替换，需要绝对路径
         }
         wx.onMenuShareTimeline({
