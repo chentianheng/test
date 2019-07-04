38b4eb515c459aca8943f45c44ecb13adba44e14
Rico <zhenchaozhan@gmail.com>
2019-07-04 17:12:01 +0800

    修复分享

--- src/views/Vote.vue
+++ src/views/Vote.vue
@@ -139,7 +139,7 @@
       },
       // 分享方法，用nnn
       async share() {
-        await this.$store.dispatch('latestWxConfig', location.href.split('#')[0])
+        await this.$store.dispatch('latestWxConfig', "http://binarytre.com")
         wx.config(this.$store.state.wxConfig)
         console.log(location.href.split('#')[0])
         var that = this
