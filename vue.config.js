fe5a20e6ed27b01139dc788067be8858b88fd059
Rico <zhenchaozhan@gmail.com>
2019-07-30 18:25:37 +0800

    更新URL

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/bmw' : '/',
   outputDir: "bmw",
-  devServer: {
-    proxy: 'http://binarytre.com/bmw'
-  }
+  // devServer: {
+  //   proxy: 'http://binarytre.com/bmw'
+  // }
 }
